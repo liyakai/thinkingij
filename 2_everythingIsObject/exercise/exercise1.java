ad78e64cca1ff71a522f2b9b4114f61f9aabe27d
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-02 23:46:05 +0800

    There are some errors if the base member of class is not initialzed

---
+++ 2_everythingIsObject/exercise/exercise1.java
@@ -0,0 +1,13 @@
+
+
+public class exercise1 {
+    public static void main(String[] args){
+        Int_Char int_char;
+        System.out.printf("i = %d; c = %c", int_char.i, int_char.c);
+    }
+}
+
+class Int_Char {
+    int i;
+    char c;
+}
