28088737d1f947e30997c397896ab572f82e793e
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-03 00:26:23 +0800

    DataOnly

---
+++ 2_everythingIsObject/exercise/exercise4.java
@@ -0,0 +1,13 @@
+
+
+public class exercise4 {
+    public static void main(String[] args){
+       DataOnly data = new DataOnly(); 
+    }
+}
+
+class DataOnly{
+    int i;
+    double d;
+    boolean b;
+}
