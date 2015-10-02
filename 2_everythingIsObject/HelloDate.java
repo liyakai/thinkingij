7a7a2d40c72a43b42a8c0d650c6cd317333d6e83
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-02 21:43:46 +0800

    change a little

--- 2_everythingIsObject/HelloDate.java
+++ 2_everythingIsObject/HelloDate.java
@@ -1,9 +1,23 @@
-//HelloDate.java
+//: HelloDate.java
 import java.util.*;
 
+/*
+ * The first Thinking in Java example program.
+ * Displays a string and today's date.
+ * @author liyakai@mail.ustc.edu.cn
+ * @author www.liyakai.cn
+ * @version 1.0
+ * */
 public class HelloDate {
+ /** Entrv point to class & application.
+ * @param args array of string arguments
+ * @throws exceptions No exceptions thrown
+ * */
     public static void main(String[] args) {
         System.out.println("Hello, it's: ");
         System.out.println(new Date());
   } 
-}
+} /* Output: (55% match)
+Hello, it's:
+Wed Oct 05 14:39:36 MDT 2015
+*///:~
