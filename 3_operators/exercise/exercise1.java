dac72431edf658438f066b312f91a862c2b43bd2
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-04 00:57:50 +0800

    normal print sentences without 'short'

---
+++ 3_operators/exercise/exercise1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+//import static net.mindview.util.Print.*;
+
+public class exercise1 {
+    public static void main(String[] args) {
+        System.out.println("I don't want to download his package.\nthe time is : ");
+        System.out.println(new Date());
+    }
+}
+
