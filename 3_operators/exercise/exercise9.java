c952a90daf280400b2a20e433e57e147866c3266
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-05 01:02:24 +0800

    Display the largest and smallest numbers for both float and double.

---
+++ 3_operators/exercise/exercise9.java
@@ -0,0 +1,14 @@
+
+public class exercise9 {
+    public static void main(String[] args) {
+        float fmax = Float.MAX_VALUE;
+        float fmin = Float.MIN_VALUE;
+        double dmax = Double.MAX_VALUE;
+        double dmin = Double.MIN_VALUE;
+        
+        System.out.println(fmax);
+        System.out.println(fmin);
+        System.out.println(dmax);
+        System.out.println(dmin);
+    }
+}
