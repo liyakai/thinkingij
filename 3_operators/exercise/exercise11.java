fbd64e58675295e4bdabe9163a1da54a24eab48e
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-05 06:33:15 +0800

    change the way to show.

--- 3_operators/exercise/exercise11.java
+++ 3_operators/exercise/exercise11.java
@@ -2,9 +2,10 @@
 public class exercise11 {
     public static void main(String[] args){
         int a = 0x8000;
+        System.out.println("a: " + Integer.toBinaryString(a));
         while (a != 0) {
-            System.out.println("a: " + Integer.toBinaryString(a));
             a >>= 1;
+            System.out.println("a: " + Integer.toBinaryString(a));
         }
     }
 }
