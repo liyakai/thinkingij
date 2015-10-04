110f2e363e9932281a7a4e1d5a5fbed69f98cb4e
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-05 00:57:56 +0800

    hex and octal notations work with long calues.

---
+++ 3_operators/exercise/exercise8.java
@@ -0,0 +1,9 @@
+
+public class exercise8 {
+    public static void main(String[] args) {
+        long l1 = 0x2f;
+        long l2 = 065;
+        long l3 = l1 + l2;
+        System.out.println("l3 = " + Long.toBinaryString(l3));
+    }
+}
