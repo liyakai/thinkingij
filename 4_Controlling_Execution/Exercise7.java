3b9e837bc3e4ad66d0ceffe7907d4d4a4abb944e
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-08 01:24:08 +0800

    use break, return

---
+++ 4_Controlling_Execution/Exercise7.java
@@ -0,0 +1,11 @@
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++){
+            if (i == 99)
+              //  return; 
+                break;
+            System.out.println("i = " + i);
+         }
+    }
+}
