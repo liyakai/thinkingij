e5ce784abec427fc0ffc805241fa114c0a9a1e7c
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-08 21:45:43 +0800

    use switch

---
+++ 4_Controlling_Execution/Exercise8.java
@@ -0,0 +1,23 @@
+public class Exercise8{
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            switch(i) {
+                case 0:
+                    System.out.println("This is 0");
+                    break;
+                case 1:
+                    System.out.println("This is 1");
+                    break;
+                case 2:
+                    System.out.println("This is 2");
+                    break;
+                case 3:
+                    System.out.println("This is 3");
+                    break;
+                case 4:
+                    System.out.println("This is 4");
+                    break;
+            }
+        }
+    }
+}
