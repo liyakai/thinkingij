43cbe64fd096237dacc79591260395a4e74cf4e3
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-09 04:56:51 +0800

    default constructor

---
+++ 5_Initialization_Cleanup/Exercise3.java
@@ -0,0 +1,11 @@
+public class Exercise3 {
+    public static void main(String[] args){
+        new AClass();
+    }
+}
+
+class AClass {
+    AClass(){
+        System.out.println("This is AClass's constructor");
+    }
+}
