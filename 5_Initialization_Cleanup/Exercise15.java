67948db782be614ce6247caaec92b6fc7842c27f
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-10 06:42:55 +0800

    In fact, I don't understand the theme really.

---
+++ 5_Initialization_Cleanup/Exercise15.java
@@ -0,0 +1,13 @@
+class Dog {
+    String s;
+    Dog(){
+        s = "hello";
+        System.out.println("s: " + s);
+    }
+}
+
+public class Exercise15 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+    }
+}
