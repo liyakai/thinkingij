3738d7b8b2ebc8314496f36c43064da8d1617267
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-10 16:16:15 +0800

    array of String

---
+++ 5_Initialization_Cleanup/Exercise16.java
@@ -0,0 +1,12 @@
+public class Exercise16 {
+    public static void main(String[] args) {
+        String[] str = new String[5];
+        str[0] = "ni";
+        str[1] = "hao";
+        str[2] = "a";
+        str[3] = "Hello";
+        str[4] = "World";
+        for (String s: str)
+            System.out.println("s: " + s);
+    }
+}
