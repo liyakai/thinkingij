5f545fc80d76fd2ea411af6b33f833542ce58d08
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-10 20:26:34 +0800

    It doesn't print.

---
+++ 5_Initialization_Cleanup/Exercise17.java
@@ -0,0 +1,13 @@
+class Dog {
+    String s;
+    Dog(String s) {
+        this.s = s;
+        System.out.println("s: " + s);
+    }
+}
+
+public class Exercise17 {
+    public static void main(String[] args) {
+        Dog[] str = new Dog[5];
+    }
+}
