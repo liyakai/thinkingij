e0b6c26682f2d0edb4b3c62c859571e84da61225
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-11 15:32:31 +0800

    Create a main() that uses varargs instead of the ordinary main()

---
+++ 5_Initialization_Cleanup/Exercise20.java
@@ -0,0 +1,8 @@
+public class Exercise20 {
+    public static void main(String... args) {
+        System.out.println("srgs: ");
+        for (String s : args)
+            System.out.print(s + " " );
+        System.out.println();
+    }
+}
