c55697b8876c839fce11e26d7766e4991fda1914
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-21 14:15:35 +0800

    abstruct class can't new a object

---
+++ 9_Interfaces/Exercise2.java
@@ -0,0 +1,11 @@
+abstract class Dog {
+    Dog(){
+        System.out.println("Cteate a dog.");
+    }
+}
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        new Dog();
+    }
+}
