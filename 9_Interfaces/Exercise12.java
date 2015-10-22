b74a30a71d7e8a22c17238ab3644f66b4f567653
liyakai <liyakai@mail.ustc.edu.cn>
2015-10-22 19:24:21 +0800

    add a interface of CanClimb

---
+++ 9_Interfaces/Exercise12.java
@@ -0,0 +1,41 @@
+interface CanFight {
+    void fight();
+}
+
+interface CanSwim {
+    void swim();
+}
+
+interface CanFly {
+    void fly();
+}
+
+interface CanClimb {
+    void climb();
+}
+
+class ActionCharacter {
+    public void fight() {}
+}
+
+class Hero extends ActionCharacter implements CanFight, CanSwim, CanFly ,CanClimb{
+    public void swim() {}
+    public void fly() {}
+    public void climb() {}
+}
+
+public class Exercise12 {
+    public static void t(CanFight x) {x.fight();}
+    public static void u(CanSwim x) {x.swim();}
+    public static void v(CanFly x) {x.fly();}
+    public static void w(ActionCharacter x) {x.fight();}
+    public static void x(CanClimb x) {x.climb();}
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        t(h);          // Treat it as a CanFight
+        u(h);          // Theat it as a CanSwim
+        v(h);          // Theat it as a CanFly
+        w(h);          // Theat it as a ActionCharacter
+        x(h);          // Theat it as a CanCLimb
+    }
+}
